61ca33a169518ba2c4cd457bd45799e021e563db
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-03-22 16:45:39 +0530

    added message receiving and sending to client

--- src/requests_handler.rs
+++ src/requests_handler.rs
@@ -64,3 +64,74 @@ pub fn handle_sent_messages(
         message.clear();
     }
 }
+
+/// Run by an unique thread started at the launch of the program.
+/// Continuously listens for connections from the receiver
+/// and forward it to every senders of the senders list (one per client)
+///
+/// # Arguments:
+///
+/// * `receiver` - Channel receiver to get messages
+/// * `senders_mutex_pointer` - Atomic reference-counting pointer for the senders array
+pub fn receive_message(
+    receiver: mpsc::Reciver<String>,
+    senders_mutex_pointer: Arc<Mutex<Vec<mpsc::Sender<String>>>>,
+) {
+    loop {
+        /* blocking listening procedure for incoming messages */
+        let message_result = reciver.recv();
+
+        /* ignore the message if this is an error result object */
+        if message_result.is_err() {
+            continue;
+        }
+         /* acquires the senders mutex, blocks until it is available */
+        let guard = senders_mutex_pointer.lock().unwrap();
+
+        /* create a reference to the senders list, first access it through
+        the pointer and then creates a reference to this array */
+        let sender = &*guard;
+
+        /* get the message from the receiver result */
+        let message = message_result.unwrap();
+
+        /* send the message to every senders into the senders array
+           (send the message to every client) */
+        for sender in senders {
+            sender.send(message.to_string())
+                .expect("cannot send message");
+        }
+    }
+}
+
+/// Run by threads created at each client connection.
+/// Each thread has a receiver that waits for data
+/// send by the client sender from the senders dynamic array;
+/// the thread forward the message into the client dedicated stream
+///
+/// # Arguments:
+///
+/// * `stream` - TCP stream between the server and the new connected client
+/// * `receiver` - Channel receiver to get messages
+pub fn send_to_client(
+    mut stream: TcpStream,
+    receiver: mpsc::Receiver<String>,
+) {
+
+    loop {
+
+        /* the client receiver listens for messages,
+           this is a blocking IO */
+        let message_result = receiver.recv();
+
+        if message_result.is_err() {
+            continue;
+        }
+
+        /* the message is pushed into the dedicated client stream
+           so the client can receive it */
+        let message = message_result.unwrap();
+        let message_bytes = message.as_bytes();
+        stream.write(message_bytes).unwrap();
+    }
+}
