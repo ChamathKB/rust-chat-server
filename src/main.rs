f33c7e3adb52187cef4e13b430d077de61a4595a
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-03-25 02:47:20 +0530

    initialized main script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+//! Basic TCP server
+
+mod requests_handler;
+
+use std::net::TcpListener;
+use std::thread::spawn
+use std::sync::{
+    Mutex,
+    Arc,
+};
+
+use std::sync::mpsc::{
+    Sender,
+    Receiver,
+    channel,
+};
+
+use requests_handler::{
+    receive_message,
+    handle_sent_messages,
+    send_to_client,
+};
+
 fn main() {
-    println!("Hello, world!");
-}
+    
+    /* create a TCP socket server, listening for connections */
+    let listener = TcpListener::bind("0.0.0.0:9090").unwrap();
+};
